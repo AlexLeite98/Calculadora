08d8f0c56ae86a9fd9345a292652c4e0c93b66aa
Alex Leite <j.alex.l.leite03@gmail.com>
2025-02-23 12:36:53 -0300

    v2 Calculadora

--- script.js
+++ script.js
@@ -1,105 +1,94 @@
-const divCalculadora = document.getElementById('divCalculadora');
-const divTabuada = document.getElementById('divTabuada');
-const calculador = document.getElementById('calculadora');
-const tabuad = document.getElementById('tabuada');
-const volta = document.getElementById('voltar');
-const divInput = document.getElementById('divInput');
 let reCal = document.getElementById('reCal');
-let reTab = document.getElementById('reTab');
+let som = document.getElementById('som');
+let sub = document.getElementById('sub');
+let mul = document.getElementById('mul');
+let div = document.getElementById('div');
 
+const a1 = document.getElementById('a1');
+const a2 = document.getElementById('a2');
 
-function calculadora(){
-    calculador.style.display = 'none'
-    tabuad.style.display = 'none'
-    divCalculadora.style.display = 'block'
-    volta.style.display = 'block'
-    divInput.style.display = 'block' 
-    reCal.style.display = 'block'
-    
+a1.addEventListener('mouseenter', entraA1);
+a1.addEventListener('mouseout', saiA1);
+
+a2.addEventListener('mouseenter', entraA2);
+a2.addEventListener('mouseout', saiA2);
+
+function entraA1() {
+    a1.style.background = 'rgb(16, 16, 16)'
 };
 
-function tabuada(){
-    tabuad.style.display = 'none'
-    calculador.style.display = 'none'
-    divTabuada.style.display = 'block'
-    volta.style.display = 'block'
-    divInput.style.display = 'block' 
-    reTab.style.display = 'block'
-}
-
-function voltar(){
-    calculador.style.display = 'block'
-    tabuad.style.display = 'block'
-    volta.style.display = 'none'
-    divCalculadora.style.display = 'none'
-    divTabuada.style.display = 'none'
-    divInput.style.display = 'none'
-    reCal.style.display = 'none'
-    reTab.style.display = 'none'
+function saiA1() {
+    a1.style.background = 'black'
 };
 
-function adicao(){
+function entraA2() {
+    a2.style.background = 'rgb(16, 16, 16)'
+};
+
+function saiA2() {
+    a2.style.background = 'black'
+};
+
+function adicao() {
     calc(1)
-    tabu(1)
 };
 
-function subtracao(){
+function subtracao() {
     calc(2)
-    tabu(2)
 };
 
-function multiplicacao(){
+function multiplicacao() {
     calc(3)
-    tabu(3)
 };
 
-function divisao(){
+function divisao() {
     calc(4)
-    tabu(4)
 };
 
-function calc(a){
+function calc(a) {
     let num1 = document.getElementById('num1').value;
     let num2 = document.getElementById('num2').value;
-    
-    if (a == 1){
+
+    if (a == 1) {
         reCal.innerHTML = (+num1) + (+num2)
     };
-    if (a == 2){
+    if (a == 2) {
         reCal.innerHTML = (+num1) - (+num2)
     };
-    if (a == 3){
+    if (a == 3) {
         reCal.innerHTML = (+num1) * (+num2)
     };
-    if (a == 4){
+    if (a == 4) {
         reCal.innerHTML = (+num1) / (+num2)
-    }; 
+    };
 };
 
-function tabu (a){
+function tabu() {
     let num = document.getElementById('num').value;
     let vezes = document.getElementById('vezes').value;
-    let c = "";
-    
-    if (a == 1){
-        for(let i = 1; i <= vezes; i++){
-            c += `<p>${i} + ${num} = ${(+i) + (+num)}</p>`  
-        };
+    let so = "";
+    let su = "";
+    let mu = "";
+    let di = "";
+
+
+    for (let i = 1; i <= vezes; i++) {
+        so += `<p>${i} + ${num} = ${(+i) + (+num)}</p>`
     };
-    if (a == 2){
-        for(let i = 1; i <= vezes; i++){
-            c += `<p>${i} - ${num} = ${(+i) - (+num)}</p>`
-        };
+    som.innerHTML = so
+
+    for (let i = 1; i <= vezes; i++) {
+        su += `<p>${i} - ${num} = ${(+i) - (+num)}</p>`
     };
-    if (a == 3){
-        for(let i = 1; i <= vezes; i++){
-            c += `<p>${i} x ${num} = ${(+i) * (+num)}</p>`
-        };
+    sub.innerHTML = su
+
+    for (let i = 1; i <= vezes; i++) {
+        mu += `<p>${i} x ${num} = ${(+i) * (+num)}</p>`
     };
-    if (a == 4){
-        for(let i = 1; i <= vezes; i++){
-            c += `<p>${i} / ${num} = ${(+i) / (+num)}</p>`
-        };
+    mul.innerHTML = mu
+
+    for (let i = 1; i <= vezes; i++) {
+        di += `<p>${i} / ${num} = ${(+i) / (+num)}</p>`
     };
-    reTab.innerHTML = c  
+    div.innerHTML = di
 };
